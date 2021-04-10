097e853e7c70b0ebcb2499a69bf5cd3df43fc3b0
Toby Furr <tfurr01@gmail.com>
2021-04-10 12:34:29 -0400

    add function to run a 5 game match / report winner

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let computerSelection;
 let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
+let ties = 0;
 
-playerPlay();
-computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+game();
 
 function computerPlay() {
     computerSelection = Math.random();
@@ -30,37 +31,65 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
             result = "You lose! Paper beats Rock!";
+            computerScore++;
         }
         else if (computerSelection === 'scissors') {
             result = "You win! Rock beats Scissors!";
+            playerScore++;
         }
         else if (computerSelection ==='rock') {
             result = "It's a tie!";
+            ties++;
         }
     }
 
     else if (playerSelection === 'paper') {
         if (computerSelection === 'paper') {
             result = "It's a tie!";
+            ties++;
         }
         else if (computerSelection === 'scissors') {
             result = "You lose! Scissors beats Paper!";
+            computerScore++;
         }
         else if (computerSelection === 'rock') {
             result = "You win! Paper beats Rock!";
+            playerScore++;
         }
     }
 
     else {
         if (computerSelection === 'paper') {
             result = "You win! Scissors beats Paper!";
+            playerScore++;
         }
         else if (computerSelection === 'rock') {
             result = "You lose! Rock beats Scissors!";
+            computerScore++;
         }
         else if (computerSelection === 'scissors') {
             result = "It's a tie!";
+            ties++;
         }
     }
     return result;
+}
+
+function game() {
+    //play 5 games, keeping track of who wins each game
+    for (let i = 1; i <= 5; i++) {
+        playerPlay();
+        computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    //compare wins vs losses and declare winner of the match
+    if (playerScore > computerScore) {
+        console.log(`You win the match ${playerScore} to ${computerScore}!`);
+    }
+    else if (playerScore < computerScore) {
+        console.log(`You lose the match ${computerScore} to ${playerScore}!`);
+    }
+    else {
+        console.log(`The match is a tie! ${playerScore} - ${computerScore} - ${ties}`);
+    }
 }
