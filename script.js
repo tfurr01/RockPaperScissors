d499abe93b00f9b4d4bb3168d4203e2175546e6f
Toby Furr <tfurr01@gmail.com>
2021-04-09 22:47:13 -0400

    successfully plays one round of rps

---
+++ script.js
@@ -0,0 +1,67 @@
+let computerSelection;
+let playerSelection;
+
+playerPlay();
+computerPlay();
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
+
+function computerPlay() {
+    computerSelection = Math.random();
+    if (computerSelection < .334) {
+        computerSelection = 'rock'
+    }
+    else if (computerSelection < .667) {
+        computerSelection = 'paper'
+    }
+    else {
+        computerSelection = 'scissors'
+    }
+}
+
+function playerPlay() {
+    playerSelection = prompt("Rock, Paper, or Scissors?");
+    playerSelection = playerSelection.toLowerCase(); 
+}
+
+function playRound(playerSelection, computerSelection) {
+    let result;
+    
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            result = "You lose! Paper beats Rock!";
+        }
+        else if (computerSelection === 'scissors') {
+            result = "You win! Rock beats Scissors!";
+        }
+        else if (computerSelection ==='rock') {
+            result = "It's a tie!";
+        }
+    }
+
+    else if (playerSelection === 'paper') {
+        if (computerSelection === 'paper') {
+            result = "It's a tie!";
+        }
+        else if (computerSelection === 'scissors') {
+            result = "You lose! Scissors beats Paper!";
+        }
+        else if (computerSelection === 'rock') {
+            result = "You win! Paper beats Rock!";
+        }
+    }
+
+    else {
+        if (computerSelection === 'paper') {
+            result = "You win! Scissors beats Paper!";
+        }
+        else if (computerSelection === 'rock') {
+            result = "You lose! Rock beats Scissors!";
+        }
+        else if (computerSelection === 'scissors') {
+            result = "It's a tie!";
+        }
+    }
+    return result;
+}
